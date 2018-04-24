cd9f4fa1aa78a92cd7ee25fc033e94f519b0a7f8
Andres F. Gonzalez <ag2018ci24@gmail.com>
2018-04-24 15:42:31 -0500

    Creacion del servidor.

--- server/app.js
+++ server/app.js
@@ -3,6 +3,10 @@ var oApp = oExpress();
 var oServer = require('http').Server(oApp);
 var oIo = require('socket.io')(oServer);
 
+oApp.get('/', function(oReq, oRes){
+	oRes.status(200).send('Hola Mundo');
+});
+
 oServer.listen(3000, function(){
 	console.log('Puerto: 3000');
 });
