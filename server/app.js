64338c62a883d716b82fbe80cbcd40d462ffc0e9
Andres F. Gonzalez <ag2018ci24@gmail.com>
2018-04-24 13:29:14 -0500

    Modificando estructura.

--- server/app.js
+++ server/app.js
@@ -1,16 +1,8 @@
 var oExpress = require('express');
 var oApp = oExpress();
-var oHttp = require("http").Server(oApp);
-var oIo = require('socket.io')(oHttp);
 
 //especificamos el subdirectorio donde se encuentran las páginas estáticas
 oApp.use(oExpress.static(__dirname + '/public'));
-
-oIo.sockets.on('connection', function(socket){
-	socket.on('message', function(data){
-		socket.broadcast.emit('enviar mensasje', data);
-	});
-});
 
 oApp.listen(3000, function(){
     console.log('Puerto: 3000');
