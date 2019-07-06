26c408c854cc3211d6a6e1ddcc94fd6cbeb721e9
Andres Gonzalez <andresg9108@gmail.com>
2019-07-06 10:25:38 -0500

    Changing file structure

--- client.js
+++ client.js
@@ -4,7 +4,8 @@ var oServer = require('http').Server(oApp);
 var oSocket = require('socket.io')(oServer);
 var iPort = 3000;
 
-oApp.use(oExpress.static(__dirname + '/'));
+oApp.use(oExpress.static(__dirname + '/public'));
+oApp.use('/jquery', oExpress.static(__dirname + '/node_modules/jquery/dist/'));
 
 oServer.listen(iPort, function(){
 	console.log('Port: '+iPort);
